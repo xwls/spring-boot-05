d7fc097a2cff35ed655af5fb4ab96359e9eeb24f
xwls <351916362@qq.com>
2018-12-11 15:50:12 +0800

    添加缓存Cacheable

--- src/main/java/com/hwua/springboot05/SpringBoot05Application.java
+++ src/main/java/com/hwua/springboot05/SpringBoot05Application.java
@@ -3,10 +3,13 @@ package com.hwua.springboot05;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 //自动扫描数据层接口
 @MapperScan("com.hwua.springboot05.dao")
+//启动缓存
+@EnableCaching
 public class SpringBoot05Application {
 
     public static void main(String[] args) {
--- src/main/java/com/hwua/springboot05/service/impl/BookServiceImpl.java
+++ src/main/java/com/hwua/springboot05/service/impl/BookServiceImpl.java
@@ -3,13 +3,19 @@ package com.hwua.springboot05.service.impl;
 import com.hwua.springboot05.dao.BookMapper;
 import com.hwua.springboot05.po.Book;
 import com.hwua.springboot05.service.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("bookService")
 public class BookServiceImpl implements BookService {
+
+    private Logger logger = LoggerFactory.getLogger(BookServiceImpl.class);
 
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
@@ -21,7 +27,9 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
+    @Cacheable(cacheNames = "book",key = "#id",condition = "#id < 10")
     public Book getBook(Integer id) {
+        logger.debug("查询"+id+"号图书");
         return bookMapper.queryById(id);
     }
 
